b17e98762c9096aff97bebdd83317d67ae8834f3
Balint Fulop <bfulop@mac.com>
2018-03-27 16:54:53 +0200

    Watch file and send request to server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const os = require('os')
 const path = require('path')
+const http = require('http')
 const R = require('ramda')
 const chokidar = require('chokidar')
 
@@ -12,6 +13,8 @@ const logger = r => {
 
 const watchTarget = R.compose(R.drop(6), R.find(R.startsWith('watch=')), R.drop(2))
 
+const regressionServer = R.compose(R.drop(7), R.find(R.startsWith('server=')), R.drop(2))(process.argv)
+
 const targetPath = R.compose(r => path.resolve(os.homedir(), r), watchTarget)(process.argv)
 
 var watcher = chokidar.watch(targetPath, {
@@ -19,6 +22,24 @@ var watcher = chokidar.watch(targetPath, {
   persistent: true
 })
 
+console.log('watching', targetPath, 'server', regressionServer)
+
 watcher.on('change', path => {
   logger(`File ${path} has been changed`)
+  http.request({
+    method: 'GET',
+    host: regressionServer,
+    port: 3202,
+    path: '/compare'
+  }, function (res) {
+    console.log(`STATUS: ${res.statusCode}`)
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+    res.setEncoding('utf8')
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`)
+    })
+    res.on('end', () => {
+      console.log('No more data in response.')
+    })
+  }).end()
 })
